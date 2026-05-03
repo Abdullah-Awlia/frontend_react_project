bdb9da6e63380e0ae6202b8eb70e7f1ff6fc61b9
Abdullah Awlia <aali.awlia@gmail.com>
2026-05-03 14:25:51 +0500

    quotes font and styling is modified

--- src/Components/Quotes.jsx
+++ src/Components/Quotes.jsx
@@ -10,11 +10,9 @@ const Quotes = () => {
                     <div className="two-quotes">
                         <div className="quote">
                             <p className="quote-line"><span>"</span>If you can change your mind, you can change your life.</p>
-                            <p className="author" >Brian Tracy</p>
                         </div>
                         <div className="quote">
                             <p className="quote-line"><span>"</span>Believe you can and you're halfway there.</p>
-                            <p className="author">Brian Tracy</p>
                         </div>
                     </div>
                 </div>
